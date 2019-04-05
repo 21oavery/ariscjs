653aedba9ac36e908f61f704eb2dd9646d40c4f0
powerboat9-school <33042784+powerboat9-school@users.noreply.github.com>
2019-04-05 12:38:03 -0400

    Delete comp.lisp

--- comp.lisp
+++
@@ -1,3 +0,0 @@
-(defmacro string-case (v) (concatenate 'string "case " v ":"))
-(defmacro string-case-lookup (k v var) (concatenate 'string (string-case k) " " var " = " v "; break;"))
-(def main (print (string-case-lookup "40" "20" "seed")))
